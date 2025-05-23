c7eea01f1106a327ab7875771c967b5182cae752
BbS19KH <44458968+BbS19KH@users.noreply.github.com>
2025-05-23 16:49:18 +0900

    Restructure project into multi-module three-tier layout

--- presentation/src/main/java/com/example/presentation/PresentationApplication.java
+++ presentation/src/main/java/com/example/presentation/PresentationApplication.java
@@ -1,11 +1,13 @@
-package com.example.demo;
+package com.example.presentation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PresentationApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(PresentationApplication.class, args);
     }
+
 }
